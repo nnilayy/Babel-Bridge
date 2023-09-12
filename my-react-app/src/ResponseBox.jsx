4ac75c03ec02d108588d8b8d386d88bb22d8e35b
unknown <nnilayy.work@gmail.com>
2023-09-13 00:53:00 +0530

    Modified Frontend Files

--- my-react-app/src/ResponseBox.jsx
+++ my-react-app/src/ResponseBox.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const ResponseBox = ({ responseText }) => {
   return (
-    <div className="response-box">
-      <h3>Server Response:</h3>
-      <p>{responseText}</p>
+    <div className='response-box-container'>
+        <div className="response-box">
+        <h3>Server Response:</h3>
+        <p>{responseText}</p>
+        </div>
     </div>
   );
 };
